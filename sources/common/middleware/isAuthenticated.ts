b5145207213a9ff8b9d44bbaff079c8e031e16cd
NotHydra <rizky.irswanda115@gmail.com>
2023-11-18 18:37:37 +0800

    feat: implement activity

--- sources/common/middleware/isAuthenticated.ts
+++ sources/common/middleware/isAuthenticated.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 export function isAuthenticated(req: Request, res: Response, next: NextFunction) {
-    req.session.userId = 1;
+    // req.session.userId = 1;
 
     if (req.session.userId) {
         next();
