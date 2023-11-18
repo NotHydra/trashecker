6d123dc7def4143777bb5f5908c98b0c9d607f9d
NotHydra <rizky.irswanda115@gmail.com>
2023-11-18 16:18:28 +0800

    feat: profile change and change password

--- sources/common/middleware/isAuthenticated.ts
+++ sources/common/middleware/isAuthenticated.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 
 export function isAuthenticated(req: Request, res: Response, next: NextFunction) {
-    // req.session.userId = 1;
+    req.session.userId = 1;
 
     if (req.session.userId) {
         next();
-    } else if (!(req.session.userId)) {
+    } else if (!req.session.userId) {
         res.redirect("/login");
     }
 }
