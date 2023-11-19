4641b54447255f500b6335428dce66b03d7ebeb5
NotHydra <rizky.irswanda115@gmail.com>
2023-11-19 10:47:10 +0800

    feat: chart

--- sources/routes/account/index.ts
+++ sources/routes/account/index.ts
@@ -16,125 +16,8 @@ const navActive = [2, 1];
 accountRouter.get("/", async (req, res) => {
     const currentYear = new Date().getFullYear();
 
-    const cardItemChild = [
-        {
-            id: 1,
-            title: "User",
-            icon: "user",
-            value: await User.countDocuments().lean(),
-            link: "account/user",
-        },
-    ];
-
     const userChartData: any = await datasetYear(User, currentYear);
-    const lineChartChild = [
-        {
-            id: 1,
-            title: "Statistik User Baru",
-            link: { link: "account/user", title: "User", subTitle: "Akun" },
-            value: userChartData.currentYearValue,
-            text: "User Baru",
-            percentage: userChartData.percentageIncrease,
-            timeRange: "Sejak Tahun Lalu",
-            dataset: userChartData.dataset,
-            firstLegend: "Tahun Ini",
-            secondLegend: "Tahun Lalu",
-        },
-    ];
-
-    const userTotal = await User.countDocuments().lean();
-    const donutChartArray = [
-        {
-            id: 1,
-            donutChartChild: [
-                {
-                    id: 1,
-                    title: "Statistik User Berdasarkan Role",
-                    link: { link: "account/user", title: "User", subTitle: "Akun" },
-                    dataset: [
-                        {
-                            id: 1,
-                            label: "Operator",
-                            value: await User.countDocuments({ role: "operator" }).lean(),
-                            color: blueColorPattern(1, 2),
-                        },
-                        {
-                            id: 2,
-                            label: "Admin",
-                            value: await User.countDocuments({ role: "admin" }).lean(),
-                            color: blueColorPattern(2, 2),
-                        },
-                    ],
-                },
-                {
-                    id: 2,
-                    title: "Statistik User Berdasarkan Status",
-                    link: { link: "account/user", title: "User", subTitle: "Akun" },
-                    dataset: [
-                        {
-                            id: 1,
-                            label: "Aktif",
-                            value: await User.countDocuments({ status: true }).lean(),
-                            color: blueColorPattern(1, 2),
-                        },
-                        {
-                            id: 2,
-                            label: "Tidak Aktif",
-                            value: await User.countDocuments({ status: false }).lean(),
-                            color: blueColorPattern(2, 2),
-                        },
-                    ],
-                },
-            ],
-        },
-    ];
-
-    if (roleCheck(app.locals.userObject.role, 4)) {
-        cardItemChild.push({
-            id: 2,
-            title: "Aktivitas",
-            icon: "eye",
-            value: await UserActivity.countDocuments().lean(),
-            link: "account/activity",
-        });
-
-        const activityChartData: any = await datasetYear(UserActivity, currentYear);
-        lineChartChild.push({
-            id: 2,
-            title: "Statistik Aktivitas Baru",
-            link: { link: "account/activity", title: "Aktivitas", subTitle: "Akun" },
-            value: activityChartData.currentYearValue,
-            text: "Aktivitas Baru",
-            percentage: activityChartData.percentageIncrease,
-            timeRange: "Sejak Tahun Lalu",
-            dataset: activityChartData.dataset,
-            firstLegend: "Tahun Ini",
-            secondLegend: "Tahun Lalu",
-        });
-
-        donutChartArray.push({
-            id: 2,
-            donutChartChild: [
-                {
-                    id: 1,
-                    title: "Statistik Aktivitas Berdasarkan User",
-                    link: { link: "account/activity", title: "Aktivitas", subTitle: "Akun" },
-                    dataset: await Promise.all(
-                        (
-                            await User.find().select("_id username").sort({ username: 1 }).lean()
-                        ).map(async (itemObject, itemIndex) => {
-                            return {
-                                id: itemIndex + 1,
-                                label: itemObject.username,
-                                value: await UserActivity.countDocuments({ idUser: itemObject._id }).lean(),
-                                color: blueColorPattern(itemIndex + 1, userTotal),
-                            };
-                        })
-                    ),
-                },
-            ],
-        });
-    }
+    const activityChartData: any = await datasetYear(UserActivity, currentYear);
 
     res.render("pages/index", {
         headTitle,
@@ -142,16 +25,100 @@ accountRouter.get("/", async (req, res) => {
         cardItemArray: [
             {
                 id: 1,
-                cardItemChild,
+                cardItemChild: [
+                    {
+                        id: 1,
+                        title: "User",
+                        icon: "user",
+                        value: await User.countDocuments().lean(),
+                        link: "account/user",
+                    },
+                    {
+                        id: 2,
+                        title: "Aktivitas User",
+                        icon: "eye",
+                        value: await UserActivity.countDocuments().lean(),
+                        link: "account/activity",
+                    },
+                ],
             },
         ],
         lineChartArray: [
             {
                 id: 1,
-                lineChartChild,
+                lineChartChild: [
+                    {
+                        id: 1,
+                        title: "Statistik User Baru",
+                        link: { link: "account/user", title: "User", subTitle: "Akun" },
+                        value: userChartData.currentYearValue,
+                        text: "User Baru",
+                        percentage: userChartData.percentageIncrease,
+                        timeRange: "Sejak Tahun Lalu",
+                        dataset: userChartData.dataset,
+                        firstLegend: "Tahun Ini",
+                        secondLegend: "Tahun Lalu",
+                    },
+                    {
+                        id: 2,
+                        title: "Statistik Aktivitas Baru",
+                        link: { link: "account/activity", title: "Aktivitas", subTitle: "Akun" },
+                        value: activityChartData.currentYearValue,
+                        text: "Aktivitas Baru",
+                        percentage: activityChartData.percentageIncrease,
+                        timeRange: "Sejak Tahun Lalu",
+                        dataset: activityChartData.dataset,
+                        firstLegend: "Tahun Ini",
+                        secondLegend: "Tahun Lalu",
+                    },
+                ],
             },
         ],
-        donutChartArray,
+        donutChartArray: [
+            {
+                id: 1,
+                donutChartChild: [
+                    {
+                        id: 1,
+                        title: "Statistik User Berdasarkan Role",
+                        link: { link: "account/user", title: "User", subTitle: "Akun" },
+                        dataset: [
+                            {
+                                id: 1,
+                                label: "Operator",
+                                value: await User.countDocuments({ role: "operator" }).lean(),
+                                color: blueColorPattern(1, 2),
+                            },
+                            {
+                                id: 2,
+                                label: "Admin",
+                                value: await User.countDocuments({ role: "admin" }).lean(),
+                                color: blueColorPattern(2, 2),
+                            },
+                        ],
+                    },
+                    {
+                        id: 2,
+                        title: "Statistik User Berdasarkan Status",
+                        link: { link: "account/user", title: "User", subTitle: "Akun" },
+                        dataset: [
+                            {
+                                id: 1,
+                                label: "Aktif",
+                                value: await User.countDocuments({ status: true }).lean(),
+                                color: blueColorPattern(1, 2),
+                            },
+                            {
+                                id: 2,
+                                label: "Tidak Aktif",
+                                value: await User.countDocuments({ status: false }).lean(),
+                                color: blueColorPattern(2, 2),
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
     });
 });
 
--- sources/routes/trash/index.ts
+++ sources/routes/trash/index.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 import { blueColorPattern, datasetYear } from "../../utility";
 import { roleGuard } from "../../authentication/guard/role.guard";
 
-import { Bin } from "../../models";
+import { Bin, BinReport } from "../../models";
 import { binTrashRouter } from "./bin";
 
 export const trashRouter = Router();
@@ -14,6 +14,7 @@ trashRouter.get("/", async (req, res) => {
     const currentYear = new Date().getFullYear();
 
     const binChartData: any = await datasetYear(Bin, currentYear);
+    const binReportChartData: any = await datasetYear(BinReport, currentYear);
 
     res.render("pages/index", {
         headTitle,
@@ -22,82 +23,160 @@ trashRouter.get("/", async (req, res) => {
         toastTitle: req.query.response == "success" ? "Login Berhasil" : "Login Gagal",
         toastText: req.query.text,
         cardItemArray: [
-            // {
-            //     id: 1,
-            //     cardItemChild: [
-            //         {
-            //             id: 1,
-            //             title: "Tempat Pembuangan Sampah",
-            //             icon: "user",
-            //             value: await TrashBin.countDocuments().lean(),
-            //             link: "trash/trash-bin",
-            //         },
-            //     ],
-            // },
+            {
+                id: 1,
+                cardItemChild: [
+                    {
+                        id: 1,
+                        title: "Tempat Pembuangan",
+                        icon: "dumpster",
+                        value: await Bin.countDocuments().lean(),
+                        link: "trash/bin",
+                    },
+                    {
+                        id: 2,
+                        title: "Penuh",
+                        icon: "circle-plus",
+                        value: await Bin.countDocuments({ status: true }).lean(),
+                        link: "trash/bin",
+                    },
+                    {
+                        id: 3,
+                        title: "Kosong",
+                        icon: "circle-minus",
+                        value: await Bin.countDocuments({ status: false }).lean(),
+                        link: "trash/bin",
+                    },
+                ],
+            },
+            {
+                id: 2,
+                cardItemChild: [
+                    {
+                        id: 1,
+                        title: "Laporan",
+                        icon: "clipboard",
+                        value: await BinReport.countDocuments().lean(),
+                        link: "trash/bin",
+                    },
+                    {
+                        id: 2,
+                        title: "Belum Direspon",
+                        icon: "circle-exclamation",
+                        value: await BinReport.countDocuments({ status: 0 }).lean(),
+                        link: "trash/bin",
+                    },
+                    {
+                        id: 3,
+                        title: "Ditolak",
+                        icon: "circle-minus",
+                        value: await BinReport.countDocuments({ status: 1 }).lean(),
+                        link: "trash/bin",
+                    },
+                    {
+                        id: 4,
+                        title: "Diterima",
+                        icon: "circle-plus",
+                        value: await BinReport.countDocuments({ status: 2 }).lean(),
+                        link: "trash/bin",
+                    },
+                    {
+                        id: 5,
+                        title: "Selesai",
+                        icon: "circle-check",
+                        value: await BinReport.countDocuments({ status: 3 }).lean(),
+                        link: "trash/bin",
+                    },
+                ],
+            },
         ],
         lineChartArray: [
-            // {
-            //     id: 1,
-            //     lineChartChild: [
-            //         {
-            //             id: 1,
-            //             title: "Statistik Tempat Pembuangan Sampah Baru",
-            //             link: { link: "trash/trash-bin", title: "Tempat Pembuangan Sampah", subTitle: "Sampah" },
-            //             value: trashBinChartData.currentYearValue,
-            //             text: "Tempat Pembuangan Sampah Baru",
-            //             percentage: trashBinChartData.percentageIncrease,
-            //             timeRange: "Sejak Tahun Lalu",
-            //             dataset: trashBinChartData.dataset,
-            //             firstLegend: "Tahun Ini",
-            //             secondLegend: "Tahun Lalu",
-            //         },
-            //     ],
-            // },
+            {
+                id: 1,
+                lineChartChild: [
+                    {
+                        id: 1,
+                        title: "Statistik Tempat Pembuangan Baru",
+                        link: { link: "trash/bin", title: "Tempat Pembuangan", subTitle: "Sampah" },
+                        value: binChartData.currentYearValue,
+                        text: "Tempat Pembuangan Baru",
+                        percentage: binChartData.percentageIncrease,
+                        timeRange: "Sejak Tahun Lalu",
+                        dataset: binChartData.dataset,
+                        firstLegend: "Tahun Ini",
+                        secondLegend: "Tahun Lalu",
+                    },
+                    {
+                        id: 2,
+                        title: "Statistik Laporan Tempat Pembuangan Baru",
+                        link: { link: "trash/bin", title: "Laporan Tempat Pembuangan", subTitle: "Sampah" },
+                        value: binReportChartData.currentYearValue,
+                        text: "Laporan Tempat Pembuangan Baru",
+                        percentage: binReportChartData.percentageIncrease,
+                        timeRange: "Sejak Tahun Lalu",
+                        dataset: binReportChartData.dataset,
+                        firstLegend: "Tahun Ini",
+                        secondLegend: "Tahun Lalu",
+                    },
+                ],
+            },
         ],
         donutChartArray: [
-            // {
-            //     id: 1,
-            //     donutChartChild: [
-            //         {
-            //             id: 1,
-            //             title: "Statistik User Berdasarkan Role",
-            //             link: { link: "account/user", title: "User", subTitle: "Akun" },
-            //             dataset: [
-            //                 {
-            //                     id: 1,
-            //                     label: "Operator",
-            //                     value: await User.countDocuments({ role: "operator" }).lean(),
-            //                     color: blueColorPattern(1, 2),
-            //                 },
-            //                 {
-            //                     id: 2,
-            //                     label: "Admin",
-            //                     value: await User.countDocuments({ role: "admin" }).lean(),
-            //                     color: blueColorPattern(2, 2),
-            //                 },
-            //             ],
-            //         },
-            //         {
-            //             id: 2,
-            //             title: "Statistik User Berdasarkan Status",
-            //             link: { link: "account/user", title: "User", subTitle: "Akun" },
-            //             dataset: [
-            //                 {
-            //                     id: 1,
-            //                     label: "Aktif",
-            //                     value: await User.countDocuments({ status: true }).lean(),
-            //                     color: blueColorPattern(1, 2),
-            //                 },
-            //                 {
-            //                     id: 2,
-            //                     label: "Tidak Aktif",
-            //                     value: await User.countDocuments({ status: false }).lean(),
-            //                     color: blueColorPattern(2, 2),
-            //                 },
-            //             ],
-            //         },
-            //     ],
-            // },
+            {
+                id: 1,
+                donutChartChild: [
+                    {
+                        id: 1,
+                        title: "Statistik Tempat Pembuangan Berdasarkan Status",
+                        link: { link: "trash/bin", title: "Tempat Pembuangan", subTitle: "Sampah" },
+                        dataset: [
+                            {
+                                id: 1,
+                                label: "Penuh",
+                                value: await Bin.countDocuments({ status: true }).lean(),
+                                color: blueColorPattern(1, 2),
+                            },
+                            {
+                                id: 2,
+                                label: "Kosong",
+                                value: await Bin.countDocuments({ status: false }).lean(),
+                                color: blueColorPattern(2, 2),
+                            },
+                        ],
+                    },
+                    {
+                        id: 2,
+                        title: "Statistik Laporan Berdasarkan Status",
+                        link: { link: "trash/bin", title: "Laporan Tempat Pembuangan", subTitle: "Sampah" },
+                        dataset: [
+                            {
+                                id: 1,
+                                label: "Belum Direspon",
+                                value: await BinReport.countDocuments({ status: 0 }).lean(),
+                                color: blueColorPattern(1, 4),
+                            },
+                            {
+                                id: 2,
+                                label: "Ditolak",
+                                value: await BinReport.countDocuments({ status: 1 }).lean(),
+                                color: blueColorPattern(2, 4),
+                            },
+                            {
+                                id: 3,
+                                label: "Diterima",
+                                value: await BinReport.countDocuments({ status: 2 }).lean(),
+                                color: blueColorPattern(3, 4),
+                            },
+                            {
+                                id: 4,
+                                label: "Selesai",
+                                value: await BinReport.countDocuments({ status: 3 }).lean(),
+                                color: blueColorPattern(4, 4),
+                            },
+                        ],
+                    },
+                ],
+            },
         ],
     });
 });
