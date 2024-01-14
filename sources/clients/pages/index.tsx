a1ab36a9005c312dbf65dd1cb6d4e5b6f43ebfa4
NotHydra <rizky.irswanda115@gmail.com>
2024-01-15 02:12:41 +0800

    feat: web overhaul

--- sources/clients/pages/index.tsx
+++ sources/clients/pages/index.tsx
@@ -1,12 +1,58 @@
-import Link from "next/link";
+import Head from "next/head";
+import { useEffect, useState } from "react";
 
-const IndexPage = (): JSX.Element => (
-  <>
-    <h1>Trashecker</h1>
+const IndexPage = (): JSX.Element => {
+  const [data, setData] = useState<any>(null);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const request = await fetch("https://trashecker.irswanda.com/trash-bin");
+                const response = await request.json();
 
-    <Link href="/">Home</Link>
-    <Link href="/trash-bin">Trash Bin</Link>
-  </>
-);
+                setData(response);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        const intervalId = setInterval(fetchData, 500);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+  return (<>
+    <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+
+        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/swiper-bundle.min.css" />
+
+        <link rel="shortcut icon" href="img/icon.png" type="image/x-icon" />
+        <title>Trashecker | Home</title>
+    </Head>
+
+    <>
+        <section className="home" id="home">
+            <div className="home-text">
+                <h1>Trash<span>ecker</span></h1>
+                <h6>Trash <span>Checker</span> status is currently <span>{data ? (data.full === true ? "Full" : "Empty") : "Empty"}</span></h6>
+                <p>In collaboration with <span>Informatika</span> x <span>Desain Komunikasi Visual</span></p>
+            </div>
+
+            <div className="home-img">
+                <img src="img/profile.png" alt="Profile" />
+            </div>
+        </section>
+    </>
+  </>)
+};
 
 export default IndexPage;
+
