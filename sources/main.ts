ea97605f79c9520b2b768a74a46abe41b90798a6
NotHydra <rizky.irswanda115@gmail.com>
2024-01-14 22:07:27 +0800

    fix: import

--- sources/main.ts
+++ sources/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   dotenv.config();
