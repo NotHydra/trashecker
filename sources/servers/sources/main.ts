7278130a19b20ed7dfb3341d40a636653e694a6f
NotHydra <rizky.irswanda115@gmail.com>
2024-01-15 00:52:57 +0800

    fix: cors attempt #2

--- sources/servers/sources/main.ts
+++ sources/servers/sources/main.ts
@@ -5,8 +5,29 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
 
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+
+  app.use(
+    (
+      req: any,
+      res: { header: (arg0: string, arg1: string) => void },
+      next: () => void,
+    ) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+      next();
+    },
+  );
+
+  app.enableCors({
+    credentials: true,
+    origin: '*',
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,POST,PUT,DELETE',
+  });
 
   await app.listen(port);
 
