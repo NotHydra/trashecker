04e3860e22f1c3e65cce6fbab8090df9c4759c4b
NotHydra <rizky.irswanda115@gmail.com>
2024-01-26 00:18:04 +0800

    feat: servers websocket #2

--- sources/servers/sources/providers/socket.gateway.ts
+++ sources/servers/sources/providers/socket.gateway.ts
@@ -1,8 +1,13 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server } from "socket.io";
 
 @WebSocketGateway()
 export class SocketGateway {
     @WebSocketServer()
     server: Server;
+
+    @SubscribeMessage("trashBinChanged")
+    handleMessage(@MessageBody() trashBin: TrashBinDTOInterface): void {
+        this.server.emit("trashBinChanged", trashBin);
+    }
 }
